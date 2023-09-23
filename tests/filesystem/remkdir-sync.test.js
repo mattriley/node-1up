91e55c856d447516270fca52badef36df5a4685d
Matt Riley <m@ttriley.dev>
2023-09-24 09:55:28 +1000

    Unspecified changes

---
+++ tests/filesystem/remkdir-sync.test.js
@@ -0,0 +1,27 @@
+module.exports = ({ test, assert, compose }) => () => {
+
+    test('remkdir sync', () => {
+        let done = false;
+        const targetPath = 'foo/bar.json';
+        const overrides = {
+            io: {
+                fs: {
+                    rmSync: (path, { recursive, force }) => {
+                        assert.equal(path, targetPath);
+                        assert.equal(recursive, true);
+                        assert.equal(force, true);
+                    },
+                    mkdirSync: (path, { recursive }) => {
+                        assert.equal(path, targetPath);
+                        assert.equal(recursive, true);
+                        done = true;
+                    }
+                }
+            }
+        }
+        const { fs } = compose({ overrides });
+        fs.remkdirSync(targetPath);
+        assert.ok(done);
+    });
+
+};
