ba3f5fd8343f15640a586be543fb00ddfb94512c
Matt Riley <m@ttriley.dev>
2023-09-23 10:42:09 +1000

    Unspecified changes

--- src/modules/function/index.js
+++ src/modules/function/index.js
@@ -1,4 +1,7 @@
 module.exports = {
+    invokeOrReturn: require('./invoke-or-return'),
+    isPlainFunction: require('./is-plain-function'),
     pipe: require('./pipe'),
+    pipeAssign: require('./pipe-assign'),
     pipeAsync: require('./pipe-async')
 };
--- src/modules/object/index.js
+++ src/modules/object/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     deepFreeze: require('./deep-freeze'),
     flatMapKeys: require('./flat-map-keys'),
+    flatten: require('./flatten'),
     mapKeys: require('./map-keys'),
     mapValues: require('./map-values')
 };
