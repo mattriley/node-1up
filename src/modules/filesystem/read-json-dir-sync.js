da81a98de1276012de9728502716c33901c6441d
Matt Riley <m@ttriley.dev>
2023-12-12 09:24:18 +1100

    Unspecified changes

--- src/modules/filesystem/read-json-dir-sync.js
+++ src/modules/filesystem/read-json-dir-sync.js
@@ -2,11 +2,12 @@ const nodepath = require('node:path');
 
 module.exports = ({ io, fs }) => path => {
 
-    const files = io.fs.readdirSync(path);
+    const files = io.fs.readdirSync(path).map(file => nodepath.join(path, file));
 
     const entries = files.map(file => {
         const { name, ext } = nodepath.parse(file);
-        const read = ext === '.json' ? fs.readJsonSync : io.fs.readFileSync;
+        const readNonJson = file => io.fs.readFileSync(file, 'utf8');
+        const read = ext === '.json' ? fs.readJsonSync : readNonJson;
         return [name, read(file)];
     });
 
