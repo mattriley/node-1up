23899e7c1f079fca1e7aadad8dd6527ada0f1e29
Matt Riley <m@ttriley.dev>
2025-06-18 11:45:16 +1000

    Unspecified changes

--- tests/geo/lookup-region.test.js
+++ tests/geo/lookup-region.test.js
@@ -150,6 +150,22 @@ module.exports = ({ test, assert }) => ({ geo }) => {
         assert.deepEqual(actual, expected);
     });
 
+    test('Los Angeles, (missing), US', () => {
+        const location = { city: 'Los Angeles', country: 'US' };
+        const defaultLocation = { country: 'AU' };
+
+        const expected = {
+            'country.iso2': 'US',
+            'state.iso': 'CA',
+            city: 'Los Angeles',
+            country: 'United States',
+            state: 'California'
+        }
+
+        const actual = geo.lookupRegion(location, defaultLocation);
+        assert.deepEqual(actual, expected);
+    });
+
     test('Los Angeles, CA, US', () => {
         const location = { city: 'Los Angeles', country: 'US', state: 'CA' };
         const defaultLocation = { country: 'AU' };
