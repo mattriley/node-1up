38772bf1708fc39f4ad9fd8bc7864bb6436db75d
Matt Riley <m@ttriley.dev>
2025-06-19 22:01:21 +1000

    Unspecified changes

--- src/modules/geo/find-airport.js
+++ src/modules/geo/find-airport.js
@@ -1,12 +1,7 @@
+const { getAirportByIata } = require("aircodes");
+
 module.exports = () => iataCode => {
 
-    let airportsByCode; // Lazy load
-
-    if (!airportsByCode) {
-        const airports = require('../../data/airports.json');
-        airportsByCode = _.keyBy(airports, airport => airport.iata.toLowerCase());
-    }
-
-    return airportsByCode[iataCode];
+    return getAirportByIata(iataCode.toUpperCase())
 
 };
