de6c840ca7897516b15a685b826fe3d85f54d60d
Matt Riley <m@ttriley.dev>
2025-06-21 08:49:47 +1000

    Unspecified changes

--- src/modules/arr/join.js
+++ src/modules/arr/join.js
@@ -1,8 +1,8 @@
 // Last optimised on 20 June 2025.
 
-module.exports = () => (arr, sep = ',', finalSep = sep) => {
+module.exports = ({ config }) => (arr, delimiter = config.delimiter, finalDelimiter = delimiter) => {
 
     if (arr.length <= 1) return arr[0] || '';
-    return arr.slice(0, -1).join(sep) + finalSep + arr[arr.length - 1];
+    return arr.slice(0, -1).join(delimiter) + finalDelimiter + arr[arr.length - 1];
 
 };
