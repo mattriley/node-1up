cc2270aa266379aa523351b628e294b977af134b
Matt Riley <m@ttriley.dev>
2025-06-25 09:09:43 +1000

    Unspecified changes

--- src/modules/pipe/with.js
+++ src/modules/pipe/with.js
@@ -2,8 +2,8 @@ module.exports = ({ self }) => (args, impl) => {
 
     const { steps, defaultContext, stateKey } = self.cleanArgs(...args);
 
-    return (initial = {}, context) => {
-        context ??= defaultContext;
+    return (initial = {}, context = {}) => {
+        context = { ...defaultContext, ...context };
         const state = initial;
         Object.assign(context, { [stateKey]: state });
         return impl(steps, state, context);
