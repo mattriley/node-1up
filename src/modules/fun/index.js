5a564a28d5be729f83953c544be972c37be73b4d
Matt Riley <m@ttriley.dev>
2025-06-25 20:48:08 +1000

    Unspecified changes

--- src/modules/fun/index.js
+++ src/modules/fun/index.js
@@ -3,6 +3,5 @@ module.exports = {
     invokeOrReturn: require('./invoke-or-return'),
     isPlain: require('./is-plain'),
     pipe: require('./pipe'),
-    pipeAssignWhile: require('./pipe-assign-while'),
     pipeAsync: require('./pipe-async')
 };
