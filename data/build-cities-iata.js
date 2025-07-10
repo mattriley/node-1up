e4d39d2007f365c0532b599af93e142108c2404d
Matt Riley <m@ttriley.dev>
2025-07-11 09:57:12 +1000

    Unspecified changes

--- data/build-cities-iata.js
+++ data/build-cities-iata.js
@@ -1,25 +1,40 @@
 const fs = require('fs');
+const path = require('path');
+
 const airports = require('./source/airports.json');
+const cities = require('./source/cities.json');
+
 const airportsByCity = {};
 for (const airport of airports) {
-    const cityKey = airport.city?.toLowerCase();
-    if (!cityKey) continue;
-    (airportsByCity[cityKey] ||= []).push(airport);
+    const cityName = airport.city?.toLowerCase();
+    if (cityName) {
+        (airportsByCity[cityName] ||= []).push(airport);
+    }
 }
-const findAirportsByCity = city => airportsByCity[city.toLowerCase()] ?? [];
-let cities = require('./source/cities.json');
 
-cities = cities.map(city => {
-    const airports = findAirportsByCity(city.name).filter(airport => {
+const findAirportsByCity = cityName =>
+    airportsByCity[cityName.toLowerCase()] ?? [];
+
+const getMatchingAirports = (city) =>
+    findAirportsByCity(city.name).filter(airport => {
         const [countryCode] = airport.iso_region.split('-');
         return countryCode === city.countryCode;
     });
-    const iataCodes = airports.map(airport => airport.iata);
-    const iataCode1 = iataCodes.find(code => city.name.toLowerCase().startsWith(code.toLowerCase()));
+
+const citiesIata = cities.map(city => {
+    const matchingAirports = getMatchingAirports(city);
+    const iataCodes = matchingAirports
+        .map(airport => airport.iata)
+        .filter(Boolean);
+
+    const iataCode1 = iataCodes.find(code =>
+        city.name.toLowerCase().startsWith(code.toLowerCase())
+    );
     const iataCode2 = city.countryCode === 'AU' ? iataCodes[0] : undefined;
     const iataCode = iataCode1 ?? iataCode2;
+
     return { ...city, iataCode, iataCodes };
 });
 
-const dest = __dirname + '/source/cities-iata.json';
-fs.writeFileSync(dest, JSON.stringify(cities, null, 4));
+const destPath = path.join(__dirname, 'source', 'cities-iata.json');
+fs.writeFileSync(destPath, JSON.stringify(citiesIata, null, 4));
