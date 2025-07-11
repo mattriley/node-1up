e85f8944110e9ca7d50d2969f3ae887d5e41991d
Matt Riley <m@ttriley.dev>
2025-07-11 21:15:14 +1000

    Unspecified changes

--- data/location-data.js
+++ data/location-data.js
@@ -2,10 +2,16 @@ const cities = require('./source/cities.json');
 const states = require('./source/states');
 const countries = require('./source/countries.json');
 
-const hk = states.find(s => s.name === 'Hong Kong SAR');
-if (hk) hk.name = 'Hong Kong';
+const hongkongState = states.find(s => s.name === 'Hong Kong SAR');
+if (hongkongState) hongkongState.name = 'Hong Kong';
 
-const mo = states.find(s => s.name === 'Macau SAR');
-if (mo) mo.name = 'Macau';
+const hongkongCity = cities.find(c => c.name === 'Hong Kong');
+if (hongkongCity) hongkongCity.stateCode = hongkongState.isoCode;
+
+const macauState = states.find(s => s.name === 'Macau SAR');
+if (macauState) macauState.name = 'Macau';
+
+const macauCity = cities.find(c => c.name === 'Macau');
+if (macauCity) macauCity.stateCode = macauState.isoCode;
 
 module.exports = { cities, states, countries };
