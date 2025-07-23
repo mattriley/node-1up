d721d57206b086f92c908703c1c658271fc763ce
Matt Riley <m@ttriley.dev>
2025-07-23 10:08:47 +0700

    Unspecified changes

--- src/modules/obj/json-schema/remove-duplicate-values.js
+++ src/modules/obj/json-schema/remove-duplicate-values.js
@@ -2,7 +2,7 @@ module.exports = ({ self }) => (obj, schema) => {
 
     if (!self.isPlain(obj) || !schema?.properties) return obj;
 
-    for (const [key, prop] of Object.entries(provider.schema.properties)) {
+    for (const [key, prop] of Object.entries(schema.properties)) {
         if (prop.uniqueItems) {
             obj[key] = _.uniq(obj[key]);
         }
