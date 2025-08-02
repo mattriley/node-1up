1fe268b0c4284f0bee5256cf355491b92a186d68
Matt Riley <m@ttriley.dev>
2025-08-02 23:33:52 +1000

    Unspecified changes

--- src/modules/exif/gps-date-to-iso.js
+++ src/modules/exif/gps-date-to-iso.js
@@ -1,45 +1,8 @@
-const { DateTime } = require('luxon');
+module.exports = () => (gpsDate, gpsTime) => {
 
-module.exports = ({ obj }) => args => {
-
-    args.data ??= {};
-    args.data.exif ??= {};
-    args.dateSource ??= 'exif.GPSDateStamp';
-    args.timeSource ??= 'exif.GPSTimeStamp';
-
-    const { data, dateSource, timeSource, timezone } = args;
-
-    let sourceDate = obj.dig(data, dateSource);
-    if (!sourceDate) return { dateSource, error: 'Date Source not found' };
-
-    const sourceTime = obj.dig(data, timeSource);
-    if (!sourceTime) return { timeSource, error: 'Time Source not found' };
-
-    // Normalize date string (EXIF format: "YYYY:MM:DD")
-    const sourceDateAsIso = sourceDate.replaceAll(':', '-');
-
-    // Convert time array to HH:MM:SS
-    const [h = 0, m = 0, s = 0] = sourceTime.map(Math.floor);
-    const sourceTimeAsIso = [h, m, s].map(n => String(n).padStart(2, '0')).join(':');
-    const sourceDateTimeAsIso = `${sourceDateAsIso}T${sourceTimeAsIso}`;
-
-    const timezoneForLuxon = timezone ?? 'utc';
-
-    const debug = {
-        dateSource,
-        sourceDate,
-        sourceDateAsIso,
-        timeSource,
-        sourceTime,
-        sourceTimeAsIso,
-        sourceDateTimeAsIso,
-        timezoneForLuxon
-    }
-
-    const dt = DateTime.fromISO(sourceDateTimeAsIso, { zone: timezoneForLuxon });
-    if (!dt.isValid) return { error: dt.invalidExplanation, debug };
-
-    const iso = timezone ? dt.toISO({ suppressMilliseconds: true }) : dt.toFormat("yyyy-MM-dd'T'HH:mm:ss");
-    return { iso, timezone, debug };
+    const gpsDateAsIso = gpsDate.replaceAll(':', '-');
+    const [h = 0, m = 0, s = 0] = gpsTime.map(Math.floor);
+    const gpsTimeAsIso = [h, m, s].map(n => String(n).padStart(2, '0')).join(':');
+    return `${gpsDateAsIso}T${gpsTimeAsIso}`;
 
 };
