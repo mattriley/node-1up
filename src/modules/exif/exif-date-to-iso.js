6da8d5c952e25062c45280d2933935c7f83e2581
Matt Riley <m@ttriley.dev>
2025-08-02 23:12:21 +1000

    Unspecified changes

--- src/modules/exif/exif-date-to-iso.js
+++ src/modules/exif/exif-date-to-iso.js
@@ -1,10 +1,7 @@
-const toIso = sourceDateTime => {
-    let [date, time] = sourceDateTime.split(' ');
+module.exports = () => exifDate => {
+
+    let [date, time] = exifDate.split(' ');
     date = date.replaceAll(':', '-');
     return `${date}T${time}`;
-};
 
-module.exports = $ => ({ data, sources, timezoneSource }) => {
-    sources ??= ['exif.DateTimeOriginal'];
-    return $.date.batchParse({ data, sources, timezoneSource, toIso });
 };
