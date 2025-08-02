9605875fe45264545bb04c508e15708dfde78222
Matt Riley <m@ttriley.dev>
2025-08-03 00:07:02 +1000

    Unspecified changes

--- src/modules/exif/index.js
+++ src/modules/exif/index.js
@@ -3,7 +3,6 @@ module.exports = {
     durationInSeconds: require('./duration-in-seconds'),
     exifDateToIso: require('./exif-date-to-iso'),
     gpsDateToIso: require('./gps-date-to-iso'),
-    isGpsTimeActuallyLocal: require('./is-gps-time-actually-local'),
     isGpsTimeBroken: require('./is-gps-time-broken'),
     isProbablySmartDevice: require('./is-probably-smart-device'),
     isVideo: require('./is-video'),
