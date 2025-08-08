5fa7c786202dc9c9dac82d4a92f2a2b8117a5644
Matt Riley <m@ttriley.dev>
2025-08-08 21:49:12 +1000

    Unspecified changes

--- src/modules/fun/parse-config.js
+++ src/modules/fun/parse-config.js
@@ -1,5 +1,4 @@
 module.exports = ({ is }) => (defaults = {}, config = {}) => {
-
     if (!is.plainObject(defaults)) {
         throw new Error(`[parseConfig] "defaults" must be a plain object. Received: ${JSON.stringify(defaults)}`);
     }
@@ -9,35 +8,47 @@ module.exports = ({ is }) => (defaults = {}, config = {}) => {
     }
 
     const defaultKeys = Object.keys(defaults);
+    const defaultKeySet = new Set(defaultKeys);
 
     return (args = []) => {
         if (!Array.isArray(args)) {
             throw new Error(`[parseConfig] "options" must be an array. Received: ${typeof args}`);
         }
 
-        const input = [...args];
         let overrides = {};
+        let input = args;
 
-        const last = input[input.length - 1];
+        const last = args[args.length - 1];
+
         if (is.plainObject(last)) {
             const keys = Object.keys(last);
-            const matchesDefaults = keys.every(k => defaultKeys.includes(k));
-            const hasRelevantKey = keys.some(k => defaultKeys.includes(k));
 
-            if (keys.length && matchesDefaults && hasRelevantKey) {
-                overrides = input.pop(); // treat as options
+            if (keys.length > 0) {
+                let matchCount = 0;
+
+                for (const key of keys) {
+                    if (!defaultKeySet.has(key)) {
+                        matchCount = -1;
+                        break;
+                    }
+                    matchCount++;
+                }
+
+                if (matchCount > 0) {
+                    overrides = last;
+                    input = args.slice(0, -1);
+                }
             }
         }
 
         if (input.length > defaultKeys.length) {
-            const extras = input.slice(defaultKeys.length);
-            throw new Error(`[parseConfig] Too many positional options: ${JSON.stringify(extras)}`);
+            throw new Error(`[parseConfig] Too many positional options: ${JSON.stringify(input.slice(defaultKeys.length))}`);
         }
 
-        const positional = input.reduce((acc, val, i) => {
-            acc[defaultKeys[i]] = val;
-            return acc;
-        }, {});
+        const positional = {};
+        for (let i = 0; i < input.length; i++) {
+            positional[defaultKeys[i]] = input[i];
+        }
 
         return {
             ...defaults,
@@ -46,5 +57,4 @@ module.exports = ({ is }) => (defaults = {}, config = {}) => {
             ...overrides
         };
     };
-
 };
