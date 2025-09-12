6e7ae4ae7d3e7f75b78e339addba2f52ecdb92f1
Matt Riley <m@ttriley.dev>
2025-09-12 15:59:56 +1000

    Unspecified changes

--- src/modules/path/steps.js
+++ src/modules/path/steps.js
@@ -1,5 +1,15 @@
-module.exports = ({ arr }) => (pathname, sep = require('path').sep) => {
+const { sep: DEFAULT_SEP } = require('path');
 
-    return arr.steps(pathname.split(sep)).map(step => step.join(sep));
+module.exports = () => (pathname, sep = DEFAULT_SEP) => {
 
+    const parts = pathname.split(sep).filter(Boolean); // ignore empty parts
+    const prefixes = [];
+    let acc = '';
+
+    for (const part of parts) {
+        acc = acc ? acc + sep + part : part;
+        prefixes.push(acc);
+    }
+
+    return prefixes;
 };
