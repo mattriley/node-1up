cca85f3ace82a6790eafc6709981fe206d53aa23
Matt Riley <m@ttriley.dev>
2025-09-12 10:54:51 +1000

    Unspecified changes

--- src/modules/str/join.js
+++ src/modules/str/join.js
@@ -1,7 +1,7 @@
 module.exports = ({ self }) => {
 
-    const join = self.joinConfigure();
-    join.configure = self.joinConfigure;
-    return join;
+    const configure = self.joinConfigure;
+    const join = configure();
+    return Object.assign(join, { configure });
 
 };
