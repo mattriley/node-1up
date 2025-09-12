3e8e5a829f1130b6a0f73c87d99c61d885f4629a
Matt Riley <m@ttriley.dev>
2025-09-12 10:45:11 +1000

    Unspecified changes

--- src/modules/obj/compact.js
+++ src/modules/obj/compact.js
@@ -2,9 +2,8 @@ module.exports = ({ self }) => {
 
     const configure = self.compactConfigure;
     const compact = configure({ mutate: false });
-    compact.mut = configure({ mutate: true });
-    compact.configure = self.compactConfigure;
-    return compact;
+    const mut = configure({ mutate: true });
+    return Object.assign(compact, { configure, mut });
 
 };
 
