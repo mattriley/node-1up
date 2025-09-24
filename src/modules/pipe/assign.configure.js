b1ae6169ef257954ecfc2ad6353cdb2af55b78ed
Matt Riley <m@ttriley.dev>
2025-09-25 08:59:20 +1000

    Unspecified changes

--- src/modules/pipe/assign.configure.js
+++ src/modules/pipe/assign.configure.js
@@ -1,7 +1,17 @@
-module.exports = ({ self }) => config => (...args) => {
+module.exports = ({ self }) => config => {
 
-    return self.with({ args }, ({ state, stepResult }) => {
-        return Object.assign(state ?? {}, stepResult);
-    });
+    const defaults = { defer: false };
+    const parseOptions = fun.parseConfig(defaults, config);
+
+    return (...args) => {
+        const { defer } = parseOptions();
+
+        const exec = self.with({ args }, ({ state, stepResult }) => {
+            return Object.assign(state ?? {}, stepResult);
+        });
+
+        return defer ? exec : exec();
+
+    };
 
 };
