45070a65ee08738dd5a90ee699b34cf3ec5fea61
Matt Riley <m@ttriley.dev>
2025-09-25 20:00:46 +1000

    Unspecified changes

--- src/modules/pipe/index.js
+++ src/modules/pipe/index.js
@@ -1,11 +1,11 @@
 module.exports = {
     assignConfigure: require('./assign.configure'),
-    _pipe: require('./_pipe'),
     assignAsync: require('./assign-async'),
     assignWhile: require('./assign-while'),
     assign: require('./assign'),
     async: require('./async'),
     core: require('./core'),
     merge: require('./merge'),
+    pipe: require('./pipe'),
     setup: require('./setup')
 };
