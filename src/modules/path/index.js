90e817eb464e5ac7fb90951bdb3a5a2e2ed65cc6
Matt Riley <m@ttriley.dev>
2025-09-28 22:38:12 +1000

    Unspecified changes

--- src/modules/path/index.js
+++ src/modules/path/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     insertBeforeLastConfigure: require('./insert-before-last.configure'),
     objectTreeConfigure: require('./object-tree.configure'),
+    appendFileCounters: require('./append-file-counters'),
     insertBeforeLast: require('./insert-before-last'),
     objectTree: require('./object-tree'),
     steps: require('./steps'),
