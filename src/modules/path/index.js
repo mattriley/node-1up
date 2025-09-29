4e150cd366eee6b8cff7ef662b7d52d54f3f4c1e
Matt Riley <m@ttriley.dev>
2025-09-29 22:23:14 +1000

    Unspecified changes

--- src/modules/path/index.js
+++ src/modules/path/index.js
@@ -3,6 +3,7 @@ module.exports = {
     insertFileCountersConfigure: require('./insert-file-counters.configure'),
     insertSequenceNumbersConfigure: require('./insert-sequence-numbers.configure'),
     objectTreeConfigure: require('./object-tree.configure'),
+    stepsConfigure: require('./steps.configure'),
     insertBeforeLast: require('./insert-before-last'),
     insertFileCounters: require('./insert-file-counters'),
     insertSequenceNumbers: require('./insert-sequence-numbers'),
---
+++ src/modules/path/steps.configure.js
@@ -0,0 +1,18 @@
+module.exports = () => config => {
+    const parseOptions = $.fun.parseConfig($.defaults.path, config);
+
+    return (pathname, options) => {
+        const { delimiter } = parseOptions(options);
+
+        const parts = pathname.split(delimiter).filter(Boolean); // ignore empty parts
+        const prefixes = [];
+        let acc = '';
+
+        for (const part of parts) {
+            acc = acc ? acc + delimiter + part : part;
+            prefixes.push(acc);
+        }
+
+        return prefixes;
+    };
+};
